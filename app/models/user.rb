34e65becdd3a5e062e12cd75fd58cdfcd52dabd6
Nikolay Zhurilo <nikolay.i.zhurilo@gmail.com>
2020-10-21 13:30:20 +0300

    added bootstrap and views events

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,14 @@
+require 'openssl'
+
 class User < ApplicationRecord
+  DIGEST = OpenSSL::Digest::SHA256.new
+
+  has_many :events
+
+  validates :name, presence: true, length: {maximum: 35}
+
+  validates :email, presence: true,
+                    uniqueness: true,
+                    format: { with: URI::MailTo::EMAIL_REGEXP }
+
 end
