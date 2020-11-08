effd3573eed2c7ab814ab061bfeb90e09775a1f1
Nikolay Zhurilo <nikolay.i.zhurilo@gmail.com>
2020-11-08 13:36:32 +0300

    footer fixed-down errors

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -3,6 +3,26 @@ module ApplicationHelper
     user&.avatar&.url || asset_pack_path('media/images/user_avatar.jpg')
   end
 
+# Аналогично user_avatar, только возвращает миниатюрную версию
+  def user_avatar_thumb(user)
+    user.avatar.file.present? ? user.avatar.thumb.url : asset_path('user.png')
+  end
+
+  # Возвращает адерс рандомной фотки события, если есть хотя бы одна. Или ссылку
+  # на дефолтную картинку.
+  def event_photo(event)
+    photos = event.photos.persisted
+
+    photos.any? ? photos.sample.photo.url : asset_path('event.jpg')
+  end
+
+  # Аналогично event_photo, только возвращает миниатюрную версию
+  def event_thumb(event)
+    photos = event.photos.persisted
+
+    photos.any? ? photos.sample.photo.thumb.url : asset_path('event_thumb.jpg')
+  end
+
   def fa_icon(icon_class)
     content_tag 'span', '', class: "fa fa-#{icon_class}"
   end
