b958f7dadfb5fa76cfa3c2f28ee4f1c4f9319a9b
Nikolay Zhurilo <nikolay.i.zhurilo@gmail.com>
2020-11-21 23:24:21 +0300

    added keys to .env

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -69,8 +69,8 @@ Rails.application.configure do
   config.action_mailer.smtp_settings = {
     address: 'smtp.gmail.com',
     port: '587',
-    user_name: '-', # не используйте для тестов свои реальные ящики
-    password: '-', # не храните здесь пароль!
+    user_name: "#{ENV['USER_NAME']}", # не используйте для тестов свои реальные ящики
+    password: "#{ENV['USER_PASSWORD']}",
     authentication: 'plain',
     enable_starttls_auto: true
   }
